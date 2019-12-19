ed391a0cfde510c2ffb76dfdb88fa685cf851683
akrm19 <akrm19@gmail.com>
2019-12-18 22:27:16 -0600

    submitt working version of the drumkit

--- src/components/Key/index.js
+++ src/components/Key/index.js
@@ -1,18 +1,34 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './styles.css';
 
 const Key = (props) => {
   const audioRef = useRef(null);
+  const firstRender = useRef(true);
+  const [keyClassName, setKeyClassName] = useState(`key${props.playSound ? ' playing' : ''}`);
 
   useEffect(() => {
+    if(firstRender.current) {
+      firstRender.current = false;
+      return;
+    }
+
     if(props.playSound) {
       audioRef.current.currentTime = 0; 
     }
     audioRef.current.play();
+    setKeyClassName('key playing');
   },[props.playSound])
 
+  const transitionEnded = (e) => {
+    if(e.propertyName !== 'transform') return;
+    console.log(`Transition ended!`);
+    console.log(e);
+
+    setKeyClassName('key');
+  }
+
   return ( 
-    <div data-key={props.keyNum} className="key">
+    <div data-key={props.keyNum} className={keyClassName} onTransitionEnd={transitionEnded} >
       <div className="kbd">{props.letter}</div>
       <span className="sound">{props.soundName}</span>
       <audio ref={audioRef} data-key={props.keyNum} src={props.sound}></audio>
