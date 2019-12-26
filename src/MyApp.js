6ec62c82ccec7440c3fde60e0264d1c252d054ca
akrm19 <akrm19@gmail.com>
2019-12-26 17:02:13 -0600

    Update background img to it does not repeat. remove extra comma comment out unused code

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Game from './components/Game';
-import Header from './components/Header';
-import Footer from './components/Footer';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
 import './MyApp.css';
 
 const MyApp = () => {
